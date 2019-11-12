7649e3b705d668b947c79781afdca75272c5c45e
opilane <opilane@KLASSA108-15>
2019-11-12 15:40:17 +0200

    Add service for the todo item

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -2,15 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreTodo.Services;
 using Microsoft.AspNetCore.Mvc;
 
-namespace AspNetCoreTodo.Controller 
+namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller 
     {
-        public IActionResult Index() 
-        {
+        private readonly ITodoItemService _todoItemService;
 
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+
+        public async Task<IActionResult> Index() 
+        {
+            var items = await _todoItemService.GetIncompleteItemAsync();
         }
     }
 }
