95226310364a5ae32391472b0498fe1e93276dee
opilane <opilane@KLASSA108-15>
2019-11-19 15:35:43 +0200

    Add login service and other stuff

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -1,23 +1,33 @@
+using System;
 using System.Threading.Tasks;
 using AspNetCoreTodo.Models;
 using AspNetCoreTodo.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreTodo.Controllers
 {
+    [Authorize]
     public class TodoController : Controller 
     {
         private readonly ITodoItemService _todoItemService;
+        private readonly UserManager<IdentityUser> _userManager;
 
         public TodoController(
-            ITodoItemService todoItemService)
+            ITodoItemService todoItemService,
+            UserManager<IdentityUser> userManager)
         {
             _todoItemService = todoItemService;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> Index()
         {
-            var todoItems = await _todoItemService.GetIncompleteItemsAsync();
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null) return Challenge();
+
+            var todoItems = await _todoItemService.GetIncompleteItemsAsync(currentUser);
 
             var model = new TodoViewModel()
             {
@@ -35,10 +45,37 @@ namespace AspNetCoreTodo.Controllers
                 return RedirectToAction("Index");
             }
 
-            var successful = await _todoItemService.AddItemAsync(newItem);
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null) return Challenge();
+
+            var successful = await _todoItemService
+                .AddItemAsync(newItem, currentUser);
+            
             if (!successful)
             {
                 return BadRequest("Could not add item.");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> MarkDone(Guid id)
+        {
+            if (id == Guid.Empty) 
+            {
+                return RedirectToAction("Index");
+            }
+
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null) return Challenge();
+
+            var successful = await _todoItemService
+                .MarkDoneAsync(id, currentUser);
+
+            if (!successful) 
+            {
+                return BadRequest("Could not mark item as done.");
             }
 
             return RedirectToAction("Index");
--- AspNetCoreTodo/Services/TodoItemService.cs
+++ AspNetCoreTodo/Services/TodoItemService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AspNetCoreTodo.Data;
 using AspNetCoreTodo.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCoreTodo.Services 
@@ -16,20 +17,39 @@ namespace AspNetCoreTodo.Services
             _context = context;
         }
 
-        public async Task<TodoItem[]> GetIncompleteItemsAsync() 
+        public async Task<TodoItem[]> GetIncompleteItemsAsync(
+            IdentityUser user
+        ) 
         {
             return await _context.Items
-                .Where(x => x.IsDone == false)
+                .Where(x => x.IsDone == false && x.UserID == user.Id)
                 .ToArrayAsync();
         }
 
-        public async Task<bool> AddItemAsync(TodoItem newItem) 
+        public async Task<bool> AddItemAsync(
+            TodoItem newItem, IdentityUser user) 
         {
             newItem.Id = Guid.NewGuid();
             newItem.IsDone = false;
+            newItem.UserID = user.Id;
 
             _context.Items.Add(newItem);
             
+            var saveResult = await _context.SaveChangesAsync();
+            return saveResult == 1;
+        }
+
+        public async Task<bool> MarkDoneAsync(
+            Guid id, IdentityUser user) 
+        {
+            var item = await _context.Items
+                .Where(x => x.Id == id && x.UserID == user.Id)
+                .SingleOrDefaultAsync();
+
+            if (item == null) return false;
+
+            item.IsDone = true;
+
             var saveResult = await _context.SaveChangesAsync();
             return saveResult == 1;
         }
